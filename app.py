bf568734befd0cfb0c0ae4cca5b2ffbc4dac2922
Fat-Frumos <lokankara@i.ua>
2023-04-12 13:54:32 +0300

    Added env

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 import os
+import telebot
 from flask import Flask, request
-import telegram
 
-app = Flask(__name__)
-bot = telegram.Bot(token=os.environ['1016015048:AAECjJ8bVoWFiTPtmSuxIS3mStC3EtPzBbY'])
+TOKEN = '1016015048:AAECjJ8bVoWFiTPtmSuxIS3mStC3EtPzBbY'
+APP_URL = f'https://bot-ews2.onrender.com/{TOKEN}'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
 def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-    chat_id = update.message.chat_id
-    text = update.message.text
-    bot.send_message(chat_id=chat_id, text=text)
-    return 'OK'
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    
