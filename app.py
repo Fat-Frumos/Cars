39acd5a37d36e281a626f95f9bbbd9be5652005d
Fat-Frumos <lokankara@i.ua>
2023-04-25 12:35:30 +0300

    update app

--- app.py
+++ app.py
@@ -1,36 +1,44 @@
-import os
-
 from flask import Flask, jsonify, request
-from nltk.tree import Tree
+import nltk
 
 app = Flask(__name__)
 
 @app.route('/paraphrase', methods=['GET'])
 def paraphrase():
 
-    tree_string = request.args.get('tree', '')
-    tree = Tree.fromstring(tree_string)
-    limit = int(request.args.get('limit', 20))
-    nps = [subtree for subtree in tree.subtrees(lambda t: t.label() == 'NP')]
+    tree_str = request.args.get('tree')
+    if not tree_str:
+        return jsonify({'error': 'tree parameter is required'}), 400
+
+
+    try:
+        tree = nltk.Tree.fromstring(tree_str)
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+
+    limit = request.args.get('limit', default=20, type=int)
+
+
+    nps = []
+    for subtree in tree.subtrees():
+        if subtree.label() == 'NP':
+            nps.append(subtree)
+
 
     paraphrases = []
     for i, np in enumerate(nps):
-        for j in range(i + 1, len(nps)):
+        for j in range(i+1, len(nps)):
             new_tree = tree.copy(deep=True)
-            new_np = nps[j].copy(deep=True)
-            np_index = list(new_tree.leaf_treeposition(np[0].label()))
-            np_index = np_index.index(int(np_index[-1]))
-            new_np_index = new_tree.leaf_treeposition(new_np[0].label())
-            new_tree[0][np_index[:-1]][np_index[-1]] = new_np
-            new_tree[0][new_np_index[:-1]][int(new_np_index[-1])] = np
-            paraphrases.append(str(new_tree))
+            np1, np2 = new_tree.leaf_treeposition(np.leaves()[0]), new_tree.leaf_treeposition(nps[j].leaves()[0])
+            new_tree[np1[:-1]][np1[-1]] = nps[j]
+            new_tree[np2[:-1]][np2[-1]] = np
+            paraphrase_str = str(new_tree)
+            if paraphrase_str not in paraphrases and len(paraphrases) < limit:
+                paraphrases.append(paraphrase_str)
 
-    try:
-        tree_string = request.args.get('tree')
-        tree = Tree.fromstring(tree_string)
-        return jsonify({'paraphrase': str(tree)})
-    except (ValueError, TypeError):
-        return jsonify({'error': 'Invalid input'}), 400
+
+    return jsonify({'paraphrases': [{'tree': p} for p in paraphrases]}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
