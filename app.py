ea02dbbf468111747a8a1b6623a08a3f56d714a1
Fat-Frumos <lokankara@i.ua>
2023-04-25 13:28:07 +0300

    fix tree

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import itertools
 from flask import Flask, request, jsonify
 from nltk.tree import Tree
 
@@ -5,71 +6,91 @@ app = Flask(__name__)
 
 @app.route('/paraphrase', methods=['GET'])
 def paraphrase():
-    # Get the required 'tree' parameter from the query string
+
     tree_str = request.args.get('tree')
     if not tree_str:
         return jsonify({'error': 'Missing "tree" parameter.'}), 400
     
-    # Parse the tree string into an nltk Tree object
     try:
         tree = Tree.fromstring(tree_str)
     except ValueError:
         return jsonify({'error': 'Invalid tree string.'}), 400
     
-    # Get the optional 'limit' parameter from the query string
     limit = int(request.args.get('limit', 20))
-    
-    # Find all NPs in the tree
+
     nps = find_nps(tree)
-    
-    # Generate paraphrased versions by swapping child NPs
     paraphrases = generate_paraphrases(tree, nps, limit)
     
-    # Return the paraphrases as a list in JSON format
-    return jsonify({'paraphrases': [{'tree': str(p)} for p in paraphrases]})
 
-
+    return jsonify({'paraphrases': list(paraphrases)})
+    
 def find_nps(tree):
-    nps = []
-    for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
-        np_str = ' '.join(subtree.leaves())
-        nps.append(np_str)
-    return nps
-
+    return [' '.join(subtree.leaves()) for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP')]
 
 def generate_paraphrases(tree, nps, limit):
     paraphrases = set()
-    for i, np1 in enumerate(nps):
-        for j, np2 in enumerate(nps[i+1:], i+1):
-            new_tree = tree.copy(deep=True)
-            np1_subtree, np2_subtree = find_np_subtrees(new_tree, np1, np2)
-            if np1_subtree and np2_subtree:
-                swap_nps(np1_subtree, np2_subtree)
-                paraphrased_tree = new_tree
-                paraphrases.add(paraphrased_tree)
-                if len(paraphrases) >= limit:
-                    return paraphrases
-    return paraphrases
-
+    for np1, np2 in itertools.combinations(nps, 2):
+        new_tree = tree.copy(deep=True)
+        np1_subtree, np2_subtree = find_np_subtrees(new_tree, np1, np2)
+        if np1_subtree and np2_subtree:
+            swap_nps(np1_subtree, np2_subtree)
+            paraphrased_tree = str(new_tree)
+            paraphrases.add(paraphrased_tree)
+            if len(paraphrases) >= limit:
+                break
+    return list(paraphrases)
 
 def find_np_subtrees(tree, np1, np2):
-    np1_subtree = None
-    np2_subtree = None
-    for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
-        np_str = ' '.join(subtree.leaves())
-        if np_str == np1:
-            np1_subtree = subtree
-        elif np_str == np2:
-            np2_subtree = subtree
-        if np1_subtree and np2_subtree:
-            break
-    return np1_subtree, np2_subtree
+    nps = [subtree for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP') if ' '.join(subtree.leaves()) in [np1, np2]]
+    return (nps[0], nps[1]) if len(nps) == 2 else (None, None)
+
+def swap_nps(np1_subtree, np2_subtree):
+    np1_subtree[:], np2_subtree[:] = np2_subtree[:], np1_subtree[:]
 
 
 def swap_nps(np1_subtree, np2_subtree):
-    np1_subtree_parent = np1_subtree.parent()
-    np2_subtree_parent = np2_subtree.parent()
-    np1_subtree_index = np1_subtree_parent.index(np1_subtree)
-    np2_subtree_index = np2_subtree_parent.index(np2_subtree)
-    np1_subtree_parent[np1_subtree_index] = np2_subtree
-    np2_subtree_parent[np2_subtree_index] = np1_subtree
+    np1_subtree.clear()
+    np1_subtree.extend(np2_subtree)
+    np2_subtree.clear()
+    np2_subtree.extend(np1_subtree)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+# def find_np_subtrees(tree, np1, np2):
+#     np1_subtree = None
+#     np2_subtree = None
+#     for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
+#         np_str = ' '.join(subtree.leaves())
+#         if np_str == np1:
+#             np1_subtree = subtree
+#         elif np_str == np2:
+#             np2_subtree = subtree
+#         if np1_subtree and np2_subtree:
+#             break
+#     return np1_subtree, np2_subtree
+
+# def find_nps(tree):
+#     nps = []
+#     for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
+#         np_str = ' '.join(subtree.leaves())
+#         nps.append(np_str)
+#     return nps
+
+
+# def generate_paraphrases(tree, nps, limit):
+#     paraphrases = set()
+#     for i, np1 in enumerate(nps):
+#         for j, np2 in enumerate(nps[i+1:], i+1):
+#             new_tree = tree.copy(deep=True)
+#             np1_subtree, np2_subtree = find_np_subtrees(new_tree, np1, np2)
+#             if np1_subtree and np2_subtree:
+#                 swap_nps(np1_subtree, np2_subtree)
+#                 paraphrased_tree = str(new_tree)
+#                 paraphrases.add(paraphrased_tree)
+#                 if len(paraphrases) >= limit:
+#                     return paraphrases
+#     return paraphrases
+
