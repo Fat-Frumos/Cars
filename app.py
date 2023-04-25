2c7505ea5239c800f83303c0e9dd5d48e5bc31de
Fat-Frumos <lokankara@i.ua>
2023-04-25 13:22:24 +0300

    fix json tree

--- app.py
+++ app.py
@@ -5,24 +5,29 @@ app = Flask(__name__)
 
 @app.route('/paraphrase', methods=['GET'])
 def paraphrase():
-
+    # Get the required 'tree' parameter from the query string
     tree_str = request.args.get('tree')
     if not tree_str:
         return jsonify({'error': 'Missing "tree" parameter.'}), 400
     
+    # Parse the tree string into an nltk Tree object
     try:
         tree = Tree.fromstring(tree_str)
     except ValueError:
         return jsonify({'error': 'Invalid tree string.'}), 400
     
+    # Get the optional 'limit' parameter from the query string
     limit = int(request.args.get('limit', 20))
-
+    
+    # Find all NPs in the tree
     nps = find_nps(tree)
+    
+    # Generate paraphrased versions by swapping child NPs
     paraphrases = generate_paraphrases(tree, nps, limit)
     
+    # Return the paraphrases as a list in JSON format
+    return jsonify({'paraphrases': [{'tree': str(p)} for p in paraphrases]})
 
-    return jsonify({'paraphrases': list(paraphrases)})
-    
 
 def find_nps(tree):
     nps = []
@@ -40,7 +45,7 @@ def generate_paraphrases(tree, nps, limit):
             np1_subtree, np2_subtree = find_np_subtrees(new_tree, np1, np2)
             if np1_subtree and np2_subtree:
                 swap_nps(np1_subtree, np2_subtree)
-                paraphrased_tree = str(new_tree)
+                paraphrased_tree = new_tree
                 paraphrases.add(paraphrased_tree)
                 if len(paraphrases) >= limit:
                     return paraphrases
@@ -62,10 +67,9 @@ def find_np_subtrees(tree, np1, np2):
 
 
 def swap_nps(np1_subtree, np2_subtree):
-    np1_subtree.clear()
-    np1_subtree.extend(np2_subtree)
-    np2_subtree.clear()
-    np2_subtree.extend(np1_subtree)
-
-if __name__ == '__main__':
-    app.run()
+    np1_subtree_parent = np1_subtree.parent()
+    np2_subtree_parent = np2_subtree.parent()
+    np1_subtree_index = np1_subtree_parent.index(np1_subtree)
+    np2_subtree_index = np2_subtree_parent.index(np2_subtree)
+    np1_subtree_parent[np1_subtree_index] = np2_subtree
+    np2_subtree_parent[np2_subtree_index] = np1_subtree
