0ce1105fc15a577a109e6c3a19216257bb23d632
Fat-Frumos <lokankara@i.ua>
2023-04-25 12:24:30 +0300

    cast to int

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def paraphrase():
             new_tree = tree.copy(deep=True)
             new_np = nps[j].copy(deep=True)
             np_index = list(new_tree.leaf_treeposition(np[0].label()))
-            np_index = np_index.index(np_index[-1])
+            np_index = np_index.index(int(np_index[-1]))
             new_np_index = new_tree.leaf_treeposition(new_np[0].label())
             new_tree[0][np_index[:-1]][np_index[-1]] = new_np
             new_tree[0][new_np_index[:-1]][new_np_index[-1]] = np
