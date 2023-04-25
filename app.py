15c1575942f48a47df28d5ae831ce5598669ac1a
Fat-Frumos <lokankara@i.ua>
2023-04-25 12:11:12 +0300

    fixed null

--- app.py
+++ app.py
@@ -13,17 +13,17 @@ def paraphrase():
     limit = int(request.args.get('limit', 20))
     nps = [subtree for subtree in tree.subtrees(lambda t: t.label() == 'NP')]
 
-
     paraphrases = []
     for i, np in enumerate(nps):
         for j in range(i + 1, len(nps)):
             new_tree = tree.copy(deep=True)
             new_np = nps[j].copy(deep=True)
-            np_index = new_tree.leaf_treeposition(np[0].index())
-            new_np_index = new_tree.leaf_treeposition(new_np[0].index())
+            np_index = new_tree.leaf_treeposition(np[0].label())
+            new_np_index = new_tree.leaf_treeposition(new_np[0].label())
             new_tree[0][np_index[:-1]][np_index[-1]] = new_np
             new_tree[0][new_np_index[:-1]][new_np_index[-1]] = np
             paraphrases.append(str(new_tree))
+
     try:
         tree_string = request.args.get('tree')
         tree = Tree.fromstring(tree_string)
@@ -32,4 +32,4 @@ def paraphrase():
         return jsonify({'error': 'Invalid input'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True)
