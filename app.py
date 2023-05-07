e35ad5db6330b18a53a51817ac2e905fff7ef204
Fat-Frumos <lokankara@i.ua>
2023-05-07 23:44:22 +0300

    Added forest slider Created html to mp3

--- app.py
+++ app.py
@@ -1,88 +1,82 @@
-from flask import Flask, request, jsonify
-from nltk.tree import Tree
+import requests
+import textwrap
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+from gtts import gTTS
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/paraphrase', methods=['GET'])
-def paraphrase():
+@app.route('/')
+def index():
+    query = request.args.get('query')
+    return render_template('index.html', query=query)
 
-    tree_str = request.args.get('tree')
-    if not tree_str:
-        return jsonify({'error': 'Missing "tree" parameter.'}), 400
+@app.route('/parse', methods=['POST'])
+def parse():
+        url = request.form['text']
+    with open(url, 'r') as f:
+        html_content = f.read()
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    text = soup.get_text()
+
+    language = 'en'
+    query = request.args.get('query') or "default"
+    filename = query + '.mp3'
+
+    with open(filename, "wb") as fp:
+        chunks = textwrap.wrap(text, 100)
+        with tqdm(total=len(chunks)) as pbar:
+            for chunk in chunks:
+                tts_chunk = gTTS(chunk, lang=language, tld='com', slow=False)
+                tts_chunk.write_to_fp(fp)
+                pbar.update(1)
+    return render_template('result.html', url=url, query=query)
+
+@app.route('/process', methods=['POST'])
+def process():
+    url = request.form['text']
+    response = requests.get(url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    headings = [h.get_text() for h in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])]
+    paragraphs = [p.get_text() for p in soup.find_all('p')]
+    text = ''.join(headings)
+    print(text)
+    print(paragraphs)
     
-    try:
-        tree = Tree.fromstring(tree_str)
-    except ValueError:
-        return jsonify({'error': 'Invalid tree string.'}), 400    
+    language = 'en'
+    query = request.args.get('query') or "default"
+    filename =  query + ".mp3"
 
-    limit = int(request.args.get('limit', 20))
+    with open(filename, "wb") as fp:
+        chunks = textwrap.wrap(text, 100)
+        with tqdm(total=len(chunks)) as pbar:
+            for chunk in chunks:
+                tts_chunk = gTTS(chunk, lang=language, tld='com', slow=False)
+                tts_chunk.write_to_fp(fp)
+                pbar.update(1)
+
+    return render_template('result.html', url=url, query=query)
+
+if __name__ == '__main__':
+    app.run(debug=True)
     
-    nps = find_nps(tree)
-    
-    paraphrases = generate_paraphrases(tree, nps, limit)
-    
-    return jsonify({'paraphrases': [{'tree': str(p)} for p in paraphrases]})
+  # with open(url, encoding='latin-1') as f:
+    #     html_content = f.read()
 
+# voice_language_code = 'en-US'
+# gender = 'male'    
+# html_file = 'Java.html'
+# output_file = 'Java.mp3'
 
-def find_nps(tree):
-    nps = []
-    for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
-        np_str = ' '.join(subtree.leaves())
-        nps.append(np_str)
-    return nps
+# def text_to_speech(text, language='en', gender='female'):
+#     engine = pyttsx3.init()
+#     engine.setProperty('rate', 150)
+#     engine.setProperty('voice', f'{language}-{gender}')
+#     engine.say(text)
+#     engine.runAndWait()
 
-def generate_paraphrases(tree, nps, limit):
-    paraphrases = set()
-    for i, np1 in enumerate(nps):
-        for j, np2 in enumerate(nps[i+1:], i+1):
-            new_tree = tree.copy(deep=True)
-            np1_subtree = find_np_subtree(new_tree, np1)
-            np2_subtree = find_np_subtree(new_tree, np2)
-            if np1_subtree is not None and np2_subtree is not None:
-                swap_nps(np1_subtree, np2_subtree)
-                paraphrased_tree = str(new_tree)
-                paraphrases.add(paraphrased_tree)
-                if len(paraphrases) >= limit:
-                    return paraphrases
-    return paraphrases
-
-
-def find_np_subtree(tree, np_str):
-    for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
-        if ' '.join(subtree.leaves()) == np_str:
-            return subtree
-    return None
-
-# def generate_paraphrases(tree, nps, limit):
-#     paraphrases = set()
-#     for i, np1 in enumerate(nps):
-#         for j, np2 in enumerate(nps[i+1:], i+1):
-#             new_tree = tree.copy(deep=True)
-#             np1_subtree, np2_subtree = find_np_subtrees(new_tree, np1, np2)
-#             if np1_subtree and np2_subtree:
-#                 swap_nps(np1_subtree, np2_subtree)
-#                 paraphrased_tree = str(new_tree)
-#                 paraphrases.add(paraphrased_tree)
-#                 if len(paraphrases) >= limit:
-#                     return paraphrases
-#     return paraphrases
-
-def find_np_subtrees(tree, np1, np2):
-    np1_subtree = None
-    np2_subtree = None
-    for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
-        np_str = ' '.join(subtree.leaves())
-        if np_str == np1:
-            np1_subtree = subtree
-        elif np_str == np2:
-            np2_subtree = subtree
-        if np1_subtree and np2_subtree:
-            break
-    return np1_subtree, np2_subtree
-
-def swap_nps(np1_subtree, np2_subtree):
-    np1_subtree.clear()
-    np1_subtree.extend(np2_subtree)
-    np2_subtree.clear()
-    np2_subtree.extend(np1_subtree)
-    
+# text_to_speech(text, language='en', gender='female')
---
+++ forest/mv.sh
@@ -0,0 +1,3 @@
+for i in {6..101}; do
+    mv photo_"$i"@07-05-2023_19-03-0[0-9].jpg photo_"$i".jpg
+done
